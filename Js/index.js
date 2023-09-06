6df22b0487a9568d92b8352298dc6c5beb3b3ae9
osmn_turhn <osmannuri.turhan01@gmail.com>
2023-09-06 18:35:05 +0300

    Completed search and sidebar with  part javascript

---
+++ Js/index.js
@@ -0,0 +1,60 @@
+//! home sidebar start
+
+const btnOpenSidebar = document.querySelector("#btn-menu");
+const sidebar = document.querySelector("#sidebar");
+const btnCloseSidebar = document.querySelector("#close-sidebar");
+btnOpenSidebar.addEventListener("click", function () {
+  sidebar.style.left = "0";
+});
+
+btnCloseSidebar.addEventListener("click", function () {
+  sidebar.style.left = "-100%";
+});
+
+//? click outside start
+
+document.addEventListener("click", function (event) {
+  if (
+    !event.composedPath().includes(sidebar) &&
+    !event.composedPath().includes(btnOpenSidebar)
+  ) {
+    sidebar.style.left = "-100%";
+  }
+});
+
+//? click outside end
+
+//! home sidebar end
+
+//! search modal start
+
+const btnOpenSearch = document.querySelector(".search-button");
+const btnCloseSearch = document.getElementById("close-search");
+const modalSearch = document.getElementsByClassName("modal-search");
+const modalSearchWrapper = document.getElementsByClassName("modal-wrapper");
+
+btnOpenSearch.addEventListener("click", function () {
+  modalSearch[0].style.visibility = "visible";
+  modalSearch[0].style.opacity = "1";
+});
+
+btnCloseSearch.addEventListener("click", function () {
+  modalSearch[0].style.visibility = "hidden";
+  modalSearch[0].style.opacity = "0";
+});
+
+//? click outside start
+
+document.addEventListener("click", function (e) {
+  if (
+    !e.composedPath().includes(modalSearchWrapper[0]) &&
+    !e.composedPath().includes(btnOpenSearch)
+  ) {
+    modalSearch[0].style.visibility = "hidden";
+    modalSearch[0].style.opacity = "0";
+  }
+});
+
+//? click outside end
+
+//! search modal end

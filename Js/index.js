ee793e443619455723bc9a8917d8d79a459bd93f
osmn_turhn <osmannuri.turhan01@gmail.com>
2023-11-15 17:56:56 +0300

    FixCss

--- Js/index.js
+++ Js/index.js
@@ -101,3 +101,17 @@ function showSlides(n) {
 }
 
 //! slider end
+
+//! add product to localStorage
+
+async function getData() {
+  const photos = await fetch("/Js/data.json");
+  const data = await photos.json();
+
+  data ? localStorage.setItem("products", JSON.stringify(data)) : [];
+}
+
+getData();
+
+const products = localStorage.getItem("products");
+console.log(JSON.parse(products));
